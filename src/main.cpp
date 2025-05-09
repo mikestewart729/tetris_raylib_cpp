6ae08ec2cf7bce1c471c6c163b63f4e26b7aba2e
Mike <mikestewart729@gmail.com>
2025-05-09 14:27:02 -0400

    properly seed random block selection

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include "../include/raylib.h"
 #include "game.h"
 
+#include <cstdlib>
+#include <chrono>
+
 int main()
 {
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    srand(seed);
+
     Color dark_blue { 44, 44, 127, 255 };
 
     InitWindow(300, 600, "raylib Tetris");
