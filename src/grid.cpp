61643da50dc16c21e82d63ca132f840454810440
Mike <mikestewart729@gmail.com>
2025-05-09 11:14:53 -0400

    add drawing and colors to grid class

--- src/grid.cpp
+++ src/grid.cpp
@@ -5,8 +5,12 @@ Grid::Grid()
 {
     num_rows = 20;
     num_cols = 10;
-    cell_size = 30;
+    cell_size = 15;
+    // needed to make the grid and boxes appear within the window on my machine
+    // It is unclear why (0, 0) is not the upper left corner of the window
+    vertical_offset = 300; 
     Initialize();
+    colors = GetCellColors();
 }
 
 void Grid::Initialize()
@@ -29,5 +33,37 @@ void Grid::Print()
             std::cout << grid[row][col] << ' ';
         }
         std::cout << '\n';
+    }
+}
+
+std::vector<Color> Grid::GetCellColors()
+{
+    Color dark_grey = { 26, 31, 40, 255 };
+    Color green = { 47, 230, 23, 255 };
+    Color red = { 232, 18, 18, 255 };
+    Color orange = { 226, 116, 17, 255 };
+    Color yellow = { 237, 234, 4, 255 };
+    Color purple = { 166, 0 , 247, 255 };
+    Color cyan = { 21, 204, 209, 255 };
+    Color blue = { 13, 64, 216, 255 };
+
+    return { dark_grey, green, red, orange, yellow, purple, cyan, blue };
+}
+
+void Grid::Draw() 
+{
+    for (int row { 0 }; row < num_rows; ++row)
+    {
+        for (int col { 0 }; col < num_cols; ++col)
+        {
+            int cell_value = grid[row][col];
+            DrawRectangle(
+                col * cell_size + 1, 
+                row * cell_size + 1 + vertical_offset, 
+                cell_size - 1, 
+                cell_size - 1, 
+                colors[cell_value]
+            );
+        }
     }
 }
--- src/grid.h
+++ src/grid.h
@@ -1,18 +1,26 @@
 #ifndef GRID_H
 #define GRID_H
 
+#include <vector>
+#include "../include/raylib.h"
+
 class Grid 
 {
 public:
     Grid();
     void Initialize();
     void Print();
+    void Draw();
     int grid[20][10];
 
 private:
     int num_rows;
     int num_cols;
     int cell_size;
+    int vertical_offset;
+    std::vector<Color> colors;
+
+    std::vector<Color> GetCellColors();
 };
 
 #endif
