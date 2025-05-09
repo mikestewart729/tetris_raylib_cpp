a49c26802fa6de3f3e8e0bb9b3e7806ffecca184
Mike <mikestewart729@gmail.com>
2025-05-09 14:16:55 -0400

    fix bug in IBlock rotation states definition

--- src/blocks.cpp
+++ src/blocks.cpp
@@ -27,7 +27,7 @@ IBlock::IBlock()
 {
     id = 3;
     cells[0] = { Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3) };
-    cells[1] = { Position(0, 2), Position(1, 2), Position(2, 1), Position(3, 2) };
+    cells[1] = { Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2) };
     cells[2] = { Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3) };
     cells[3] = { Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1) };
     Move(-1, 3);
@@ -37,9 +37,6 @@ OBlock::OBlock()
 {
     id = 4;
     cells[0] = { Position(0, 0), Position(1, 0), Position(0, 1), Position(1, 1) };
-    cells[1] = { Position(0, 0), Position(1, 0), Position(0, 1), Position(1, 1) };
-    cells[2] = { Position(0, 0), Position(1, 0), Position(0, 1), Position(1, 1) };
-    cells[3] = { Position(0, 0), Position(1, 0), Position(0, 1), Position(1, 1) };
     Move(0, 4);
 }
 
