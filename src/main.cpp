45caee83e1e446b56d1b18b9efe8a686908ba83d
Mike <mikestewart729@gmail.com>
2025-05-12 08:22:03 -0400

    update game UI with additional visual elements

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "../include/raylib.h"
 #include "game.h"
+#include "colors.h"
 
 #include <cstdlib>
 #include <chrono>
@@ -22,10 +23,10 @@ int main()
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     srand(seed);
 
-    Color dark_blue { 44, 44, 127, 255 };
-
     InitWindow(500, 620, "raylib Tetris");
     SetTargetFPS(60);
+
+    Font font = LoadFontEx("resources/fonts/mecha.png", 64, 0, 0);
 
     Game game { Game() };
 
@@ -39,6 +40,17 @@ int main()
 
         BeginDrawing();
         ClearBackground(dark_blue);
+
+        DrawTextEx(font, "Score", {175, 315}, 18, 2, WHITE);
+        DrawRectangleRounded({165, 335, 75, 30}, 0.3, 6, light_blue);
+
+        DrawTextEx(font, "Next", {183, 395}, 18, 2, WHITE);
+        DrawRectangleRounded({165, 415, 75, 80}, 0.3, 6, light_blue);
+
+        if (game.game_over)
+        {
+            DrawTextEx(font, "GAME\nOVER", {178, 550}, 18, 2, WHITE);
+        }
 
         game.Draw();
 
